3482654f6ef364f6c47b55711bb3c1556983ef00
Maxime Joly <maxime.joly2808@gmail.com>
2024-11-29 12:26:23 +0100

    Add files

---
+++ taxation-lib.js
@@ -0,0 +1,90 @@
+const TAX_RATES_IR = [
+  { min: 0, max: 11294, rate: 0 },
+  { min: 11294, max: 28797, rate: 0.11 },
+  { min: 28797, max: 82341, rate: 0.3 },
+  { min: 82342, max: 177106, rate: 0.41 },
+  { min: 177106, max: Infinity, rate: 0.45 },
+];
+
+function calculateIR(profit) {
+  const taxBreakdown = [];
+  let tax = 0;
+
+  for (const bracket of TAX_RATES_IR) {
+    if (profit > bracket.min) {
+      const taxableAmount = Math.min(
+        bracket.max - bracket.min,
+        profit - bracket.min
+      );
+      const taxForBracket = taxableAmount * bracket.rate;
+      tax += taxForBracket;
+
+      taxBreakdown.push({
+        range: `${bracket.min} - ${
+          bracket.max === Infinity ? "∞" : bracket.max
+        }`,
+        taxableAmount: taxableAmount.toFixed(0),
+        taxForBracket: taxForBracket.toFixed(0),
+      });
+    }
+  }
+
+  return { tax, taxBreakdown };
+}
+
+// =====================================================================================================================
+
+const PME_REVENUE_THRESHOLD = 10_000_000; // Seuil de chiffre d'affaires pour les PME
+const IS_REDUCED_TAX_RATE = 0.15; // Taux réduit de l'IS pour les PME
+const IS_STANDARD_TAX_RATE = 0.25; // Taux standard de l'IS
+
+const TAX_RATES_IS = [
+  { min: 0, max: 42500, rate: IS_REDUCED_TAX_RATE },
+  { min: 42500, max: Infinity, rate: IS_STANDARD_TAX_RATE },
+];
+
+function calculateIS(profit) {
+  let tax = 0;
+  if (profit <= PME_REVENUE_THRESHOLD) {
+    for (let { min, max, rate } of TAX_RATES_IS) {
+      if (profit > min) {
+        const taxableIncome = Math.min(profit, max) - min;
+        tax += taxableIncome * rate;
+      }
+    }
+  } else {
+    tax = profit * IS_STANDARD_TAX_RATE;
+  }
+  return tax;
+}
+
+// =====================================================================================================================
+
+// https://www.economie.gouv.fr/particuliers/decote-impot-revenu
+const DECOTE_APPLICATION_THRESHOLD = [1929, 3191]; // The threshold for applying decote for 1 and 2 parts
+const DECOTE_MAX = [873, 1444]; // The max decote amount for 1 and 2 parts
+const DECOTE_PERCENTAGE = 0.4525; // The percentage of the decote
+
+function calculteDecote(tax, familyQuotient) {
+  const decoteThreshold =
+    familyQuotient == 1
+      ? DECOTE_APPLICATION_THRESHOLD[0]
+      : DECOTE_APPLICATION_THRESHOLD[1];
+
+  const decoteMax = familyQuotient == 1 ? DECOTE_MAX[0] : DECOTE_MAX[1];
+
+  if (tax <= decoteThreshold) {
+    let decote = decoteMax - tax * DECOTE_PERCENTAGE;
+    if (tax - decote > 0) {
+      return decoteMax - tax * DECOTE_PERCENTAGE;
+    }
+  }
+  return 0;
+}
+
+// Charges sociales pour les travailleurs indépendants (TNS)
+const SOCIAL_CONTRIBUTION_RATE_TNS = 0.45; // https://entreprendre.service-public.fr/vosdroits/F36239
+const MINIMUM_SOCIAL_CONTRIBUTION_TNS = 93 + 931 + 69 + 134; // https://www.urssaf.fr/accueil/outils-documentation/taux-baremes/taux-cotisations-ac-plnr.html
+
+// Flat Tax (PFU) sur les dividendes
+const PFU_RATE = 0.3;
